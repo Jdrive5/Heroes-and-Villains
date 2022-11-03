7aeb2d4b6626afd91e8369279256dcf7d080789e
Monica Banda <monicabanda@Monicas-Air.lan>
2022-11-03 11:45:07 -0600

    Completed Project

--- supers/views.py
+++ supers/views.py
@@ -12,13 +12,16 @@ def super_list(request):
     if request.method == 'GET':
         supers_type = request.query_params.get('supers_type')
         supers = Super.objects.all()
-        
+       
 
         if supers_type:
             queryset = supers.filter(supers_types__type=supers_type)
 
-        serializer = SuperSerializer(queryset, many=True)
-        return Response (serializer.data)
+            serializer = SuperSerializer(queryset, many=True)
+            return Response (serializer.data)
+        else:
+            serializer = SuperSerializer(supers, many=True)
+            return Response (serializer.data)
     elif request.method == 'POST':
         serializer = SuperSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -38,5 +41,29 @@ def super_detail(request, pk):
     elif request.method == 'DELETE':
         super.delete()
         return Response(status=status.HTTP_204_NO_CONTENT) 
+
+
+@api_view(['GET'])
+def super_custom_dict(request):
+    all_supers = {}
+    all_supers['name'] = ''
+    print(all_supers)
+
+    supers = Super.objects.all()
+
+    custom_response = {'heroes': [], 'villians': []}
+
+
+    heroes = supers.filter(supers_types__id=1)
+    hero_serializer = SuperSerializer(heroes, many=True)
+
+    villains = supers.filter(supers_types__id=2)
+    villain_serializer = SuperSerializer(villains, many=True)
+
+    custom_response = {
+            "Hero": hero_serializer.data, "Villain": villain_serializer.data
+        }
+    return Response(custom_response)
+
 
 
